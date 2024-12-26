aa036d15e84d20048a7018572cb188581eefc6b6
Idehen Divine <idehendivine16@gmail.com>
2024-12-26 20:52:51 +0100

    edit enail before verification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Plan;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function updateProfile()
     {
         return view('profile.update');
+    }
+
+    public function updateEmailView()
+    {
+        return view('auth.update-email');
+    }
+
+    public function updateEmail(Request $request)
+    {
+        $request->validate([
+            'old-email' => 'required|email',
+            'email' => 'required|email|unique:users',
+        ]);
+
+        if (auth()->user()->email != $request->input('old-email')) {
+            return back()->withInput()->withErrors(['old-email' => 'Invalid old email']);
+        }
+
+        $user = auth()->user();
+        $user->email = $request->email;
+        $user->save();
+        return redirect()->route('dashboard');
     }
 
     public function checkins()
