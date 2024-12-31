b94ad244bb44c306cc43d736c34e1248bff8ab7e
Idehen Divine <idehendivine16@gmail.com>
2024-12-31 14:58:50 +0100

    route update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/paystack/webhook', [PaystackController::class, 'webhook'])->name('paystack.webhook');
+Route::post('/webhook/paystack', [PaystackController::class, 'webhook'])->name('paystack.webhook');
