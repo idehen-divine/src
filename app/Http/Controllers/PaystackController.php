65c340d6f4bf75d353c22d61a4503f4c6eb4c1b5
Idehen Divine <idehendivine16@gmail.com>
2024-12-31 13:44:59 +0100

    update

--- app/Http/Controllers/PaystackController.php
+++ app/Http/Controllers/PaystackController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Services\Paystack;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaystackController extends Controller
 {
     public function webhook(Request $request)
     {
+        Log::info($request->input());
         return Paystack::webhook()->charge($request);
     }
 }
